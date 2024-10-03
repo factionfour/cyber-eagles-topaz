112a6e07884c50896ff341042ff1b8de3922c2a2
Jack <spookley168@gmail.com>
2024-10-02 19:59:11 -0600

    setting up functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAutoDriveByTime_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAutoDriveByTime_Linear.java
@@ -120,7 +120,6 @@ public class RobotAutoDriveByTime_Linear extends LinearOpMode {
             telemetry.addData("Path", "Leg 2: %4.1f S Elapsed", runtime.seconds());
             telemetry.update();
         }
-
 //         Step 3:  Drive Backward for 1 Second
         frontleftDrive.setPower(-FORWARD_SPEED);
         frontrightDrive.setPower(-FORWARD_SPEED);
