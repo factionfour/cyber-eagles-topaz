91b180cf7395daef29836c865be4efb44f8a5d24
samscores <samkraiger@gmail.com>
2025-01-05 16:48:39 -0700

    worked on servo wrist had a problem with Servo .setpostion double on a null reference

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoBildaPinpointDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoBildaPinpointDriver.java
@@ -60,7 +60,6 @@ public class GoBildaPinpointDriver extends I2cDeviceSynchDevice<I2cDeviceSynchSi
     private float xVelocity    = 0;
     private float yVelocity    = 0;
     private float hVelocity    = 0;
-
     private static final float goBILDA_SWINGARM_POD = 13.26291192f; //ticks-per-mm for the goBILDA Swingarm Pod
     private static final float goBILDA_4_BAR_POD    = 19.89436789f; //ticks-per-mm for the goBILDA 4-Bar Pod
 
